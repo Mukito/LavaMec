c6b1b1779b073895780d6181af04cce9163a145c
Fabiano Santos Ramos <mukito100@gmail.com>
2024-11-05 14:55:06 -0300

    Create README.md

---
+++ README.md
@@ -0,0 +1,8 @@
+# Sistema de Mecânica e Lavagem de Carro
+
+
+![image](https://github.com/user-attachments/assets/e3fe3780-5094-4092-a4bc-19e86e5b19df)
+
+
+
+
--- clientes/views.py
+++ clientes/views.py
@@ -1,15 +1,20 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, JsonResponse
+from django.core import serializers
+from django.views.decorators.csrf import csrf_exempt
+from django.urls import reverse
+from django.shortcuts import redirect, get_object_or_404
 from .models import Cliente, Carro
+import json
 import re
-
 
 
 # Create your views here.
 def clientes(request):
     if request.method == "GET":
+        clientes_list = Cliente.objects.all()
         #return HttpResponse('Estou em clientes')
-        return render(request, 'clientes.html')
+        return render(request, 'clientes.html', {'clientes': clientes_list})
     elif request.method == "POST":
         nome = request.POST.get('nome')
         sobrenome = request.POST.get('sobrenome')
@@ -45,4 +50,66 @@ def clientes(request):
         
 
         return HttpResponse('Teste')
+    
 
+def att_cliente(request):
+    id_cliente = request.POST.get('id_cliente')
+
+    cliente = Cliente.objects.filter(id=id_cliente)
+    carros = Carro.objects.filter(cliente=cliente[0])
+    
+    clientes_json = json.loads(serializers.serialize('json', cliente))[0]['fields']
+    cliente_id = json.loads(serializers.serialize('json', cliente))[0]['pk']
+
+   
+    carros_json = json.loads(serializers.serialize('json', carros))
+    carros_json = [{'fields': carro['fields'], 'id': carro['pk']} for carro in carros_json]
+    data = {'cliente': clientes_json, 'carros': carros_json, 'cliente_id': cliente_id}
+    #print(carros_json)
+    return JsonResponse(data)
+
+@csrf_exempt
+def update_carro(request, id):
+    nome_carro = request.POST.get('carro')
+    placa = request.POST.get('placa')
+    ano = request.POST.get('ano')
+
+    carro = Carro.objects.get(id=id)
+    list_carros = Carro.objects.filter(placa=placa).exclude(id=id)
+
+    if list_carros.exists():
+        return HttpResponse('Placa já existente')
+    carro.carro = nome_carro
+    carro.placa = placa
+    carro.ano = ano
+    carro.save()
+    return HttpResponse("Dados Alterados com sucesso!")
+
+def excluir_carro(request, id):
+    try:
+        carro = Carro.objects.get(id=id)
+        carro.delete()
+        return redirect(reverse('clientes')+f'?aba=att_cliente&id_cliente={id}')
+    except:
+        #TODO
+        return redirect(reverse('clientes')+f'?aba=att_cliente&id_cliente={id}')
+    
+
+def update_cliente(request, id):
+    body = json.loads(request.body)
+    
+    nome = body['nome']
+    sobrenome = body['sobrenome']
+    email = body['email']
+    cpf = body['cpf']
+    
+    cliente = get_object_or_404(Cliente, id=id)
+    try:        
+        cliente.nome = nome
+        cliente.sobrenome = sobrenome
+        cliente.email = email
+        cliente.cpf = cpf
+        cliente.save()
+        return JsonResponse({'status': '200', 'nome': nome, 'sobrenome': sobrenome, 'email': email, 'cpf': cpf})
+    except:
+        return JsonResponse({'status': '500'})
